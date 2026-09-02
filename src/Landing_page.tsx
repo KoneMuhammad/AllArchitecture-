8b26002968cf7e59b3dc117955b9a7b7c71de4be
optimistic <salam@SALAMs-MacBook-Pro.local>
2026-09-02 17:11:29 -0400

    fix- change to name attribute in the input form`

--- src/Landing_page.tsx
+++ src/Landing_page.tsx
@@ -18,7 +18,7 @@ export function Landing_page(){
         </img>
        </header>
        <form style={{marginTop: "20px"}} onSubmit={make_request_to_github_api}>
-        <input className="input" id= "github_repository"
+        <input className="input" name="github_repository"
         type= "text" 
         placeholder="www.github repo link">
         </input>
@@ -26,22 +26,32 @@ export function Landing_page(){
        </main>
     )
 }
-function make_request_to_github_api(e){
-    fix_user_input_to_match_api_path(get_user_input(e))
+
+async function make_request_to_github_api(e){
+    const url  = fix_user_input_to_match_api_path(get_user_input(e))
+    
+    await fetch(url, {headers: {'X-GitHub-Api-Version': '2026-03-10'}} )
+
 }
 
 function fix_user_input_to_match_api_path(userRepository: any): string {
     const updatedinput1 = userRepository.replace(/blob/,"contents/blob")
     const updatedinput2 = updatedinput1.replace(/github.com/, "github.com/repos")
-    const apiPath = updatedinput2
-    return apiPath
+    const githubEndpoint = updatedinput2
+    console.log(`Final api string is: ${githubEndpoint}`)
+    return githubEndpoint
 }
 
 function get_user_input(e)
 {
     e.preventDefault();
    const form =  e.currentTarget;
-    const formdata = new FormData(form);
+   const formdata = new FormData(form);
    const userRepository = formdata.get("github_repository")
+   console.log(`User Repository input is: ${userRepository}`)
    return userRepository
 }
+
+//ts syntax
+//types of runtime errors 
+
