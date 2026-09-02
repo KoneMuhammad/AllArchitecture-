9bef29a97ca03b820375c5e9fd2398025f13fb10
optimistic <salam@SALAMs-MacBook-Pro.local>
2026-09-01 22:16:58 -0400

    1/9 amount of things added

--- src/Landing_page.tsx
+++ src/Landing_page.tsx
@@ -2,9 +2,19 @@ import landingpageimg from './assets/landingpageimg.svg'
 
 
 export function Landing_page(){
- 
-}g
-let appending = ""
+
+    let userinput: string = ''
+
+    //this is the github api path 
+    const apiCodeSample = "/repos/{owner}/{repo}/contents/{path}"
+                                           
+// here i append "repos", and "contents" to the user input
+function fix_user_input_to_match_api_path(): string {
+    const updatedinput1 = userinput.replace(/blob/,"contents/blob")
+    const updatedinput2 = updatedinput1.replace(/github.com/, "github.com/repos")
+    const apiPath = updatedinput2
+    return apiPath
+}
     return (
         <main className="main">
        <header className="header">
@@ -19,7 +29,7 @@ let appending = ""
         <img src={landingpageimg} className="headerimg">
         </img>
        </header>
-       <form style={{marginTop: "20px"}} method={HTTPMethod}action={resource}>
+       <form style={{marginTop: "20px"}} method={HTTPMethod}action={fix_user_input_to_match_api_path}>
         <input className="input" 
         type= "text" 
         placeholder="www.github repo link">
